aa0a8f29498c0f8baf2e7903f12222a03612972a
SantiRoques <santiagoroque2020@gmail.com>
2026-04-08 10:30:48 +0200

    Update bd.php

--- SiteWeb/bd.php
+++ SiteWeb/bd.php
@@ -1,6 +1,9 @@
 <?php
+	// Fonction pour se connecter à la base de données
 	function getBD(){
+		// Connexion à la base sdd4 en local
 		$bdd = new PDO('mysql:host=localhost;dbname=sdd4;charset=utf8','root','root');
+		// Retour de la connexion
 		return $bdd;
 	}
 ?>
